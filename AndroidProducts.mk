SelynCatto/rising6.0

dt: init rising

AndroidProducts.mk
@@ -1,0 +1,52 @@
+PRODUCT_MAKEFILES := \
+        $(LOCAL_DIR)/rising_arm64_bgN.mk \
+        $(LOCAL_DIR)/rising_arm64_bcgN.mk \
+        $(LOCAL_DIR)/rising_arm64_bvN.mk \
+	$(LOCAL_DIR)/treble_arm64_avS.mk \
+	$(LOCAL_DIR)/treble_arm64_avN.mk \
+	$(LOCAL_DIR)/treble_arm_avS.mk \
+	$(LOCAL_DIR)/treble_arm_avN.mk \
+	$(LOCAL_DIR)/treble_a64_avS.mk \
+	$(LOCAL_DIR)/treble_a64_avN.mk \
+	$(LOCAL_DIR)/treble_arm64_agS.mk \
+	$(LOCAL_DIR)/treble_arm64_agN.mk \
+	$(LOCAL_DIR)/treble_arm_agS.mk \
+	$(LOCAL_DIR)/treble_arm_agN.mk \
+	$(LOCAL_DIR)/treble_a64_agS.mk \
+	$(LOCAL_DIR)/treble_a64_agN.mk \
+	$(LOCAL_DIR)/treble_arm64_afS.mk \
+	$(LOCAL_DIR)/treble_arm64_afN.mk \
+	$(LOCAL_DIR)/treble_arm_afS.mk \
+	$(LOCAL_DIR)/treble_arm_afN.mk \
+	$(LOCAL_DIR)/treble_a64_afS.mk \
+	$(LOCAL_DIR)/treble_a64_afN.mk \
+	$(LOCAL_DIR)/treble_arm64_aoS.mk \
+	$(LOCAL_DIR)/treble_arm64_aoN.mk \
+	$(LOCAL_DIR)/treble_arm_aoS.mk \
+	$(LOCAL_DIR)/treble_arm_aoN.mk \
+	$(LOCAL_DIR)/treble_a64_aoS.mk \
+	$(LOCAL_DIR)/treble_a64_aoN.mk \
+	$(LOCAL_DIR)/treble_arm64_bvS.mk \
+	$(LOCAL_DIR)/treble_arm64_bvN.mk \
+	$(LOCAL_DIR)/treble_arm_bvS.mk \
+	$(LOCAL_DIR)/treble_arm_bvN.mk \
+	$(LOCAL_DIR)/treble_a64_bvS.mk \
+	$(LOCAL_DIR)/treble_a64_bvN.mk \
+	$(LOCAL_DIR)/treble_arm64_bgS.mk \
+	$(LOCAL_DIR)/treble_arm64_bgN.mk \
+	$(LOCAL_DIR)/treble_arm_bgS.mk \
+	$(LOCAL_DIR)/treble_arm_bgN.mk \
+	$(LOCAL_DIR)/treble_a64_bgS.mk \
+	$(LOCAL_DIR)/treble_a64_bgN.mk \
+	$(LOCAL_DIR)/treble_arm64_bfS.mk \
+	$(LOCAL_DIR)/treble_arm64_bfN.mk \
+	$(LOCAL_DIR)/treble_arm_bfS.mk \
+	$(LOCAL_DIR)/treble_arm_bfN.mk \
+	$(LOCAL_DIR)/treble_a64_bfS.mk \
+	$(LOCAL_DIR)/treble_a64_bfN.mk \
+	$(LOCAL_DIR)/treble_arm64_boS.mk \
+	$(LOCAL_DIR)/treble_arm64_boN.mk \
+	$(LOCAL_DIR)/treble_arm_boS.mk \
+	$(LOCAL_DIR)/treble_arm_boN.mk \
+	$(LOCAL_DIR)/treble_a64_boS.mk \
+	$(LOCAL_DIR)/treble_a64_boN.mk \

SelynCatto/rising_device_phh_treble

AndroidProducts.mk
@@ -1,0 +1,52 @@
+PRODUCT_MAKEFILES := \
+        $(LOCAL_DIR)/lineage_arm64_bgN.mk \
+        $(LOCAL_DIR)/lineage_arm64_bcgN.mk \
+        $(LOCAL_DIR)/lineage_arm64_bvN.mk \
+	$(LOCAL_DIR)/treble_arm64_avS.mk \
+	$(LOCAL_DIR)/treble_arm64_avN.mk \
+	$(LOCAL_DIR)/treble_arm_avS.mk \
+	$(LOCAL_DIR)/treble_arm_avN.mk \
+	$(LOCAL_DIR)/treble_a64_avS.mk \
+	$(LOCAL_DIR)/treble_a64_avN.mk \
+	$(LOCAL_DIR)/treble_arm64_agS.mk \
+	$(LOCAL_DIR)/treble_arm64_agN.mk \
+	$(LOCAL_DIR)/treble_arm_agS.mk \
+	$(LOCAL_DIR)/treble_arm_agN.mk \
+	$(LOCAL_DIR)/treble_a64_agS.mk \
+	$(LOCAL_DIR)/treble_a64_agN.mk \
+	$(LOCAL_DIR)/treble_arm64_afS.mk \
+	$(LOCAL_DIR)/treble_arm64_afN.mk \
+	$(LOCAL_DIR)/treble_arm_afS.mk \
+	$(LOCAL_DIR)/treble_arm_afN.mk \
+	$(LOCAL_DIR)/treble_a64_afS.mk \
+	$(LOCAL_DIR)/treble_a64_afN.mk \
+	$(LOCAL_DIR)/treble_arm64_aoS.mk \
+	$(LOCAL_DIR)/treble_arm64_aoN.mk \
+	$(LOCAL_DIR)/treble_arm_aoS.mk \
+	$(LOCAL_DIR)/treble_arm_aoN.mk \
+	$(LOCAL_DIR)/treble_a64_aoS.mk \
+	$(LOCAL_DIR)/treble_a64_aoN.mk \
+	$(LOCAL_DIR)/treble_arm64_bvS.mk \
+	$(LOCAL_DIR)/treble_arm64_bvN.mk \
+	$(LOCAL_DIR)/treble_arm_bvS.mk \
+	$(LOCAL_DIR)/treble_arm_bvN.mk \
+	$(LOCAL_DIR)/treble_a64_bvS.mk \
+	$(LOCAL_DIR)/treble_a64_bvN.mk \
+	$(LOCAL_DIR)/treble_arm64_bgS.mk \
+	$(LOCAL_DIR)/treble_arm64_bgN.mk \
+	$(LOCAL_DIR)/treble_arm_bgS.mk \
+	$(LOCAL_DIR)/treble_arm_bgN.mk \
+	$(LOCAL_DIR)/treble_a64_bgS.mk \
+	$(LOCAL_DIR)/treble_a64_bgN.mk \
+	$(LOCAL_DIR)/treble_arm64_bfS.mk \
+	$(LOCAL_DIR)/treble_arm64_bfN.mk \
+	$(LOCAL_DIR)/treble_arm_bfS.mk \
+	$(LOCAL_DIR)/treble_arm_bfN.mk \
+	$(LOCAL_DIR)/treble_a64_bfS.mk \
+	$(LOCAL_DIR)/treble_a64_bfN.mk \
+	$(LOCAL_DIR)/treble_arm64_boS.mk \
+	$(LOCAL_DIR)/treble_arm64_boN.mk \
+	$(LOCAL_DIR)/treble_arm_boS.mk \
+	$(LOCAL_DIR)/treble_arm_boN.mk \
+	$(LOCAL_DIR)/treble_a64_boS.mk \
+	$(LOCAL_DIR)/treble_a64_boN.mk \